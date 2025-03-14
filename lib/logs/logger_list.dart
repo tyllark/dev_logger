b04ba3d878d77415b9ee074db94e8995823441db
Tyler-Larkin <tyllark@amazon.com>
2025-03-13 17:39:50 -0700

    Added Auto Scrolling  for LoggerList

--- lib/logs/logger_list.dart
+++ lib/logs/logger_list.dart
@@ -1,11 +1,27 @@
 import 'package:dev_logger/logs/logger.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
-class LoggerList extends StatelessWidget {
+class LoggerList extends StatefulWidget {
   final bool reverse;
 
   const LoggerList({super.key, this.reverse = false});
+
+  @override
+  State<LoggerList> createState() => _LoggerListState();
+}
+
+class _LoggerListState extends State<LoggerList> {
+  final scrollController = ScrollController();
+  static final _dateFormatter = DateFormat('hh:mm:ss.SSSS');
+
+  @override
+  void initState() {
+    Logger.instance.addListener(_onLogAdded);
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +29,9 @@ class LoggerList extends StatelessWidget {
       listenable: Logger.instance,
       builder: (context, child) {
         return ListView.separated(
-          reverse: reverse,
+          controller: scrollController,
+          physics: ClampingScrollPhysics(),
+          reverse: widget.reverse,
           itemCount: Logger.instance.logs.length,
           separatorBuilder: (context, index) => const Divider(),
           itemBuilder: (context, index) {
@@ -21,7 +39,9 @@ class LoggerList extends StatelessWidget {
             return GestureDetector(
               onTap: () => _copyToClipboard(log.message),
               child: ListTile(
-                title: Text('${log.timestamp} - ${log.action}'),
+                title: Text(
+                  '${_dateFormatter.format(log.timestamp)} - ${log.action}',
+                ),
                 subtitle: Text(log.message),
               ),
             );
@@ -33,5 +53,19 @@ class LoggerList extends StatelessWidget {
 
   Future<void> _copyToClipboard(String text) async {
     await Clipboard.setData(ClipboardData(text: text));
+  }
+
+  void _onLogAdded() {
+    if (scrollController.position.pixels !=
+        scrollController.position.maxScrollExtent) {
+      return;
+    }
+
+    double screenHeight = MediaQuery.sizeOf(context).height;
+    scrollController.animateTo(
+      scrollController.position.maxScrollExtent + screenHeight,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+    );
   }
 }
